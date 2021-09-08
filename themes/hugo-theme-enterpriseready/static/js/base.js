38db0927e5cc18518c82f2e22a5dea82872dac07
Ted Palmer <tedpalmer@Teds-MBP.home>
2021-09-08 19:53:17 -0400

    Added back js files

--- themes/hugo-theme-enterpriseready/static/js/base.js
+++ themes/hugo-theme-enterpriseready/static/js/base.js
@@ -1,11 +1,11 @@
-// $(document).ready(function() {
-//     $("#tool-tip").hover(
-//         function() {
-//             $(".tool-tip").fadeIn(200);
-//         },
-//         function() {
-//             $(".tool-tip").fadeOut(100);
-//         }
-//     );
+$(document).ready(function() {
+    $("#tool-tip").hover(
+        function() {
+            $(".tool-tip").fadeIn(200);
+        },
+        function() {
+            $(".tool-tip").fadeOut(100);
+        }
+    );
 
-// });
+});
